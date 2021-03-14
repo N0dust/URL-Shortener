c99f0773c4ecdc266d04210fbc7c4f1cb8a46262
N0dust <44610037+N0dust@users.noreply.github.com>
2021-03-15 00:06:38 +0800

    Create middleware.go

---
+++ middleware/middleware.go
@@ -0,0 +1,37 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type MiddleWare struct {
+}
+
+
+func (m MiddleWare) LoggingHandler(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		t1 := time.Now()
+		next.ServeHTTP(w, r)
+		t2 := time.Now()
+		log.Printf("[%s] %q %v", r.Method, r.URL.String(), t2.Sub(t1))
+	}
+	return http.HandlerFunc(fn)
+
+}
+
+
+func (m MiddleWare) RecoverHandler(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				log.Printf("Recover From Panic : %+v", err)
+				http.Error(w, http.StatusText(500), 500)
+			}
+		}()
+		next.ServeHTTP(w, r)
+	}
+	return http.HandlerFunc(fn)
+
+}
